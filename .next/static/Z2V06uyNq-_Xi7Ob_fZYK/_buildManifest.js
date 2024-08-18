d14e3e094cad92242bdf74178115e72bd702da26
yousefi-p <pouya.yusefi@gmail.com>
2024-08-18 14:17:10 +0330

    acme fonts error handling

---
+++ .next/static/Z2V06uyNq-_Xi7Ob_fZYK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-65a12af0f39dd249.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ .next/static/chunks/pages/_app-a02017282cb1bee0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{3778:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(1546)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[774,179],()=>(n(3778),n(8435))),_N_E=_.O()}]);
